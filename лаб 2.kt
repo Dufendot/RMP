308a2ce6edf8bdb6d21c94e11900560a719e4a97
Akka Server <grastel001@mail.ru>
2020-04-16 20:28:34 +0300

    lab

---
+++ лаб 2.kt
@@ -0,0 +1,184 @@
+﻿//1
+fun main(args: Array<String>) {
+    val lang = if (args.size == 0) "EN" else args[0]
+    println(when (lang) {
+        "EN" -> "Hello!"
+        "FR" -> "Salut!"
+        "IT" -> "Ciao"
+        else -> " sorry, can't say in  $lang"
+    })
+}
+
+//2
+fun main(args: Array<String>) {
+    val a = 3
+    val b = 3L
+    val c = 3f
+    val d = 3.0
+    val e = "Unknown"
+    val f = "3"
+    val g = "Long"
+    val l = '3'
+    println(a as Any is Int)
+    println(b as Any is Long)
+    println(c as Any is Float)
+    println(d as Any is Double)
+    println(e as Any is String)
+    println(f as Any is String)
+    println(g as Any is String)
+    println(l as Any is Char)
+}
+
+//3
+fun main(args: Array<String>) {
+    val a = 3
+    val b = 3L
+    val c = 3f
+    val d = 3.0
+    val e = "Unknown"
+    val f = "3"
+    val g = "Long"
+    val l = '3'
+    println(describe(a))                     
+    println(describe(b))
+    println(describe(c))
+    println(describe(d))
+    println(describe(e))
+    println(describe(f))
+    println(describe(g))
+    println(describe(l))
+
+}
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+}
+
+//4
+fun main(args: Array<String>) {
+    println(describe(-5))
+    println(describe(4L))
+    println(describe(3.0))
+    println(describe("wfertykul"))
+    println(describe(4))
+    println(describe("Hello"))
+    println(describe(1))
+
+}
+
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+}
+
+//5
+fun main(args: Array<String>) {
+	println(declaration(1))
+}
+fun declaration(obj: Any):String{
+    var a = when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
+    return a
+}
+
+//6
+fun main(args:Array<String>){
+val x = 9
+when{
+isOdd(x)->println("x is odd")
+isEven(x)->println("x is even")
+else->println("x is funny")
+}
+}
+
+fun isOdd(x: Any):Boolean{
+if(x !is Int) return false
+return x%2==1
+}
+fun isEven(x: Any):Boolean{
+if (x !is Int) return false
+return x%2==1
+}
+//7
+fun main(args: Array<String>) {
+    val x = 10 
+    val y = 9 
+    if (x in 1..y+1) {
+        println("fits in range") 
+    } else {
+        println("out of range")
+    }
+}
+fun main(args: Array<String>) {
+    val x = 10 /// out of range
+    val y = 9
+    // val x = 9  /// fits in range
+    // val y = 10
+    if (x in 1..y-1) {
+        println("fits in range")
+    } else {
+        println("out of range")
+      }
+}
+
+//8
+fun main(args: Array<String>) {
+   Cup(9)
+   Cup(165)
+   Cup(21)
+   Cup(17)
+}
+fun Cup(x: Any)
+{
+    val validNumbers = arrayOf(1,523,165,999)
+    when (x) {
+        in 1..10 -> println("x is in the range")
+        in validNumbers -> println("x is valid")
+        !in 10..20 -> println("x is outside the range")
+        else -> println("none of the above")
+    }
+}
+
+//9
+fun main(args: Array<String>) {
+
+    var items = arrayOf("orange","apple")
+    //проверка массива 100% нужен apple
+    when {
+        "orange" in items -> println("juicy")
+        "apple" in items -> println("apple is fine too")
+    }
+}
+fun main(args: Array<String>) {
+
+	var items12 = arrayOf("orange","apple")
+    when {
+        "apple" in items12 -> println("apple ")
+        "orange" in items12 -> println("juicy is fine too")
+        
+    }
+}
+
+//10
+fun main(args: Array<String>) {
+
+    println(hasPrefix("array"))
+}
+
+fun hasPrefix(x: Any) = when(x) {
+    is String -> x.startsWith("arr")
+    else -> false
+}
---
+++ лаб 3.kt
@@ -0,0 +1,133 @@
+﻿//1
+/*fun main(args: Array<String>) {
+    val numbers = arrayOf<Int>(1, 2, 3, 4, 5)
+    println(numbers.indices)
+    for((index, value) in numbers.withIndex()){
+    	println("Значение индекса $index равно $value")
+    }
+ fun main(args: Array<String>) {
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+    println(numbers.indices)
+    for((index, value) in numbers.withIndex()){
+    	println("Значение индекса $index равно $value")
+    }}
+
+}*/
+
+//2
+/*fun main(args: Array<String>){
+    var arr = emptyArray<String>()
+    var i = 0
+    while(i<10){
+        arr += i.toString()
+        i++
+    }
+    println(arr.size)
+*/
+
+//3
+/*fun main(args: Array<String>){
+    var array = arrayOfNulls<String>(5)
+    array.set(0, "a")
+    array.set(1, "b")
+    array.set(2, "c")
+    array.set(3, "d")
+    array.set(4, "e")
+    println(array.get(3))
+    println(array.get(2))
+}
+}*/
+
+//4
+/*fun main(args: Array<String>){
+   val myArray = Array(7, { i -> i * 2 })
+    println(myArray[4])
+    println(myArray[6]) 
+}*/
+
+//5
+fun main(args: Array<String>){
+    val arr = arrayOf<Int>(1, 2, 3, 4, 5)
+	arr.forEach {i -> println(i)}
+}
+fun main(args: Array<String>){
+    val arr = arrayOf<Int>(1, 2, 3, 4, 5)
+	arr.forEachIndexed { index, value->println("$index - $value")
+}
+}
+
+//6
+fun main(args: Array<String>){
+    val arr = arrayOf<Int>(1, 2, 3, 4, 5)
+    var i = args[0].toInt()
+    val b = args[1].toInt()
+    while (i<b){
+        arr[i] = arr[i] * (-1)
+        i++
+    }
+    println(arr.contentToString())
+}
+
+//7
+fun main(args: Array<String>){
+    val num = setOf(1, 2, 3, -4, -55)
+    val part = num.partition{
+    	it < 0
+	}	
+    println(part)
+}
+
+//8
+fun main(args: Array<String>){
+    val a = hashSetOf(8, 12, 86, 997,1123)
+    
+    a.add(1)
+    println(a)
+    
+    println(a.contains(86))
+    
+    a.remove(8)
+    println(a)
+    
+    println(a.indices)
+    
+    println(a.count())
+    
+    a.clear()
+    println(a)
+    
+    println(a.isEmpty())
+}
+
+//9
+fun main(args: Array<String>){
+   val cliff = listOf("qwerty", "ytrewq", "!")
+   println(cliff.get(4))
+   println(cliff.size)
+   println(cliff.getOrElse(3){"Слова закончились"})
+   for(f in cliff) print (f)
+   var num = listOf(1, 2, 3, 4, 5)
+   var a = num.map{it * it} //возводит каждый элемент в квадрат
+   println(a)
+   println(num.all {it < 5}) 
+}
+}
+
+//10
+fun main(args: Array<String>){
+    val a = mapOf("beverage" to 2.7, "meal" to 12.4)
+    val b = a.mapValues{it.value.toString() + "$"}
+    println(b)
+}
+//11
+fun main(args: Array<String>){
+    val currenciesMutableMap:MutableMap<String, String> = mutableMapOf("Гривна" to "Украина", "Доллар" to "США", "Лира" to "Турция")
+    println("Страны: ${currenciesMutableMap.values}")
+    println("Валюты: ${currenciesMutableMap.keys}")
+    
+    println(currenciesMutableMap.containsKey("Гривна"))
+    
+    println(currenciesMutableMap.containsValue("Россия"))
+    
+    println(currenciesMutableMap.count())
+}
